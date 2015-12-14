c584c25f359674b9cbfabff1b37cb6bdc7081611
Liam Diprose <dotcom.diprose97@gmail.com>
2015-12-14 23:54:09 +1300

    Added more Readme

--- README.md
+++ README.md
@@ -19,3 +19,36 @@ This was added from *within* my IDE. (just checking)
     CC Turtle/Computer
        |
      Doing stuff
+
+## CC API's <http://www.computercraft.info/wiki/Category:APIs>
+
+
+## Native Turtle commands
+
+craft  | craft item from inventory
+equip  | trys to equip a tool
+go     | Move to location
+refuel | Refeul with items in inventory
+turn   | Rotate turtle
+unequip| Unequip tool
+
+
+## Server - Turtle Connection
+
+Turtles have little functionality, they ask what to do, do it,
+provide updates, then ask for a new job
+
+### Sample exchange between turtle and server:
+1. *turtle is created and script is run*
+2. Turtle connects to server, with null id.
+3. Server detects turtle is new, creates new object for turtle
+and sets ID to index in list
+4. Server adds update jobs to queue (location, inventory etc)
+5. Turtle asks for job, but job_queue is empty, server instructs turtle to do nothing (later: go to garage)
+6. Turtle continues to ask for jobs
+7. Job ID is generated (from global job count), and added to job_queue
+8. Turtle asks for job, server assigns job from queue
+9. Turtle connects with updates (if massive moveTo fucntion). If server responds with a job override, start doing new job.
+10. Turtle completes job, connects to server with complete flag and job id,
+server deletes job and replys with new job, or nothing.
+11. Repeat steps 6 - 10
--- server/server.py
+++ server/server.py
@@ -1,17 +1,51 @@
 import socket # Obviously need a network server
 import select # Allow IO multiplexing to handle multiple connections on one thread
+import collections # Immport Queues and stacks
+
+DEBUG = False
+JOB_COUNT = 0
+MACHINE_COUNT = 0
+
+def debug(message, sender):
+    if DEBUG:
+        print("[{}] {}: {}".format(time, sender, message))
+
+
+def get_job_id():
+    JOB_COUNT += 1
+    debug("Job ID {} assigned".format(JOB_COUNT))
+    return JOB_COUNT
+
+def get_machine_id():
+    MACHINE_COUNT += 1
+    debug("Machine ID assigned".format(MACHINE_COUNT))
+    return MACHINE_COUNT
 
 class Turtle(object)
     def __init__(self):
         self.location = (None, None, None)
         self.inventory = []
-        self.id = 00000  # Used to handle each turtle without getting confused
+        self.jobs = collections.deque() # Queue for jobs
+        self.current_job = 0000
+        self.id = get_machine_id()  # Used to handle each turtle without getting confused
 
-    def dig(self, direction="front"):
-        #TODO
+    def dig(self, direction="front", override=False):
+        if override:
+            # Queue Job at front of list
+        else:
+            # Queue job at back of list
 
-    def move(self, direction="forward"):
-        #TODO
+    def move(self, direction="forward", override=False):
+        if override:
+            # Queue Job at front of list
+        else:
+            # Queue job at back of list
 
-    def getSurroundings(self):
-        #TODO
+    def getSurroundings(self, override=False):
+        if override:
+            # Add current job to front of list
+            # Queue Job at front of list,
+            # Resume handing out jobs from queue
+        else:
+            # Queue job at back of list
+
